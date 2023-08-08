e06e2e8dce47ec10b96dcd4388dfbef4db70b92e
kerembkmz <101328913+kerembkmz@users.noreply.github.com>
2023-08-08 16:57:19 +0300

    More efficient solution found for climbing-the-leaderboard, still fails 2/11 tests.

--- climbing-the-leaderboard.java
+++ climbing-the-leaderboard.java
@@ -10,37 +10,49 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-
 class Result {
 
-    public static int getTheRank(List<Integer> givenArray, Integer k) {
-    int currentCount = 1;
-    for (int i = 0; i < givenArray.size(); i++) {
-        if (k.equals(givenArray.get(i))) {
-            return currentCount;
-        } else if (k < givenArray.get(i) && (i == givenArray.size() - 1 || k > givenArray.get(i + 1))) {
-            return currentCount + 1;
-        } else if (i < givenArray.size() - 1 && givenArray.get(i).equals(givenArray.get(i + 1))) {
-            continue;
-        } else if (k < givenArray.get(i)) {
-            currentCount += 1;
-        }
-    }
-    return currentCount;
-}
+    /*
+     * Complete the 'climbingLeaderboard' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY ranked
+     *  2. INTEGER_ARRAY player
+     */
+
     
+
     public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
-        List<Integer> returnList = new ArrayList<>();
-        int currentRank = 0;
-        for (int i = 0; i < player.size(); i++){
-            currentRank = new Result().getTheRank(ranked, player.get(i));
-            returnList.add(currentRank);
-        }
-        return returnList;
+    List<Integer> returnList = new ArrayList<>();
+    
+    TreeSet<Integer> mergedList = new TreeSet<>(ranked); //Remove duplicates, and order at the same time.
+    //Also helps with reordering while adding the scores in the for loop.
+    
+    //I will search the loop using a counter so that I won't be returning to the start everytime.
+    //int counter = 0;
+    //for (int score:player) {
+    //    mergedList.add(score);
+     //   List<Integer> mergedListAsList = new ArrayList<>(mergedList); //For doing binary search
+    //    Collections.reverse(mergedListAsList);
+    //    for (int i=counter; i<mergedListAsList.size(); i++){
+    //        if (mergedListAsList.get(i) == score){
+    //                returnList.add(i+1);
+     //       }
+     //   }
+    //}
+    
+    // The approach above works, however it still gets the time exceeded error.
+    for (int score : player) {
+        mergedList.add(score);
+        int playerRank = mergedList.size() - mergedList.headSet(score, true).size();
+        returnList.add(playerRank + 1);
     }
-
+    
+    return returnList;
+    
 }
-
+}
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
